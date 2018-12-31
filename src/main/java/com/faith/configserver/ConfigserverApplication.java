34f4c349369427fe0c168a5c07e3ed9046109ae9
haibo.xiong <haibo.xiong@ly.com>
2018-12-31 16:15:56 +0800

    修改配置文件路径

--- src/main/java/com/faith/configserver/ConfigserverApplication.java
+++ src/main/java/com/faith/configserver/ConfigserverApplication.java
@@ -1,11 +1,13 @@
 package com.faith.configserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableAdminServer
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
